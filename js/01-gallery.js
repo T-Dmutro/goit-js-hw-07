950f983c48c142cfe2ebcb8d578235c10d34ab2b
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2022-01-28 15:40:04 +0200

    up.galary00

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,19 +1,25 @@
 import { galleryItems } from './gallery-items.js';
-function createGalleryItem (galleryItems){
-    const markup = galleryItems.map(({preview, original, description}) => {
-        return
-        <div class="gallery__item">
-            <a class="gallery__link" href="original">
-             <img
-                class="gallery__image"
-                src="preview"
-                data-source="original"
-                alt="description"
-             />
-            </a>
-        </div>
-    });
-    console.log(markup);
-}
 
-console.log(galleryItems);
+
+
+console.log(createGalleryItem);
+
+function createGalleryItem (galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return
+       `<div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+           </a>
+        </div>`;
+    }).join('');
+      
+};
+
+
