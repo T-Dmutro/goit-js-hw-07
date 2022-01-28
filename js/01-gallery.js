2f50f9096d551c09738f0d83d77a7e11e3e2c1a3
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2022-01-28 11:35:24 +0200

    err

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+function createGalleryItem (galleryItems){
+    const markup = galleryItems.map(({preview, original, description}) => {
+        return
+        <div class="gallery__item">
+            <a class="gallery__link" href="original">
+             <img
+                class="gallery__image"
+                src="preview"
+                data-source="original"
+                alt="description"
+             />
+            </a>
+        </div>
+    });
+    console.log(markup);
+}
 
 console.log(galleryItems);
