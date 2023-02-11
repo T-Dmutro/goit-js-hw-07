e34e432d2b21729c5e94b7fcdc01e8f0fb25cb54
T-Dmutro <90419458+T-Dmutro@users.noreply.github.com>
2023-02-11 15:51:37 +0200

    task 2 start

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+
+
+
+const cartContainer = document.querySelector('.gallery');
+// console.log(cartContainer)
+cartContainer.addEventListener('click', onCardsContainerClick)
+const cardsMarkup = createCardMarkup(galleryItems);
+cartContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+
+function createCardMarkup( element){ 
+    return element.map(({preview, original, description})=>{
+        return  `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    })
+    .join(" ");}
+
+    function onCardsContainerClick(el){
+        console.log('Працює')
+        blockStandartAction(el);
+
+        let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+    }
+    function blockStandartAction(el){
+        el.preventDefault()
+    }
